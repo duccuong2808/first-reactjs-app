128424e1c9059d14ebcb6d35cddadf0ad98c92b1
Duc Cuong <duccuong2808@gmail.com>
2016-11-03 00:19:02 +0700

    router

--- main.js
+++ main.js
@@ -1,5 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<ul>
+					<li><Link to="">Home</Link></li>
+					<li><Link to="about">About</Link></li>
+					<li><Link to="contact">Contact</Link></li>
+				</ul>
+
+				{this.props.children}
+			</div>
+		)
+	}
+}
+
+export default App;
+
+class Home extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Home...</h1>
+			</div>
+		)
+	}
+}
+
+
+class About extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>About...</h1>
+			</div>
+		)
+	}
+}
+
+class Contact extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Contact...</h1>
+			</div>
+		)
+	}
+}
+
+
+ReactDOM.render((
+	<Router history = {browserHistory}>
+		<Route path = "/" component = {App}>
+			<IndexRoute component = {Home} />
+			<Route path = "home" component = {Home} />
+			<Route path = "about" component = {About} />
+			<Route path = "contact" component = {Contact} />
+		</Route>
+	</Router>
+
+), document.getElementById('app'))
