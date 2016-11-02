c94bc38fb704cd20c2598ac828847971507c4758
Duc Cuong <cuong.nd@altplus.com.vn>
2016-11-02 13:29:21 +0700

    state and prop

--- App.jsx
+++ App.jsx
@@ -1,23 +1,43 @@
 import React from 'react';
 
 class App extends React.Component {
-   constructor(props) {
-      super(props);
-      console.log(props);
-      this.state = {
-         header: "Header from state...",
-         content: "Content from state..."
-      }
-   }
+    constructor(props) {
+        super(props);
 
-   render() {
-      return (
-          <div>
-             <h1>{this.state.header}</h1>
-             <h2>{this.state.content}</h2>
-          </div>
-      );
-   }
+        this.state = {
+            header: "Header from props...",
+            "content": "Content from props..."
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Header headerProp = {this.state.header}/>
+                <Content contentProp = {this.state.content}/>
+            </div>
+        );
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.headerProp}</h1>
+            </div>
+        );
+    }
+}
+
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>{this.props.contentProp}</h2>
+            </div>
+        );
+    }
 }
 
 export default App;
